d13abb9179cc29252928dfbf2c7e91e0ff60c24a
151250088 <151250088@smail.nju.edu.cn>
2018-04-25 21:31:49 +0800

    rm

--- my_cookie.py
+++
@@ -1,2 +0,0 @@
-my_cookie_string = 'hide_filter=1; cookiesession1=2C0BC7512V0LLFHP9BZBQPFI80RDAD37; login="%E6%B1%AA%E6%B1%AA%E5%96%B5"; ' \
-            'uid="60371"; md5="%05%3B_%DA%C7J%2F%F2%B5%83%EB(e%CF%BB%26"; per_page="50" '
